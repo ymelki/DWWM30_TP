50849082c53235b80ca43aaaed019f02caba8ed0
Melki <ymelki@gmail.com>
2023-03-20 11:32:33 +0100

    Ajout_d'un_controlleur_personnalisé

--- src/Controller/DetailproduitController.php
+++ src/Controller/DetailproduitController.php
@@ -3,16 +3,30 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CommentaireRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DetailproduitController extends AbstractController
 {
     #[Route('/detailproduit/{id}', name: 'app_detailproduit')]
-    public function index(Produit $produit): Response
+    public function index(Produit $produit , CommentaireRepository $commentaireRepository): Response
     {
+        // on veut afficher les commentaires 
+        // correspondant aux produit de l'id de l'url.
+        // on utilise le repository qui va chercher avec un critere findby
+        // selon le produits 
+        dd($commentaireRepository->findAll());
+        dd($commentaireRepository->findBy([
+
+            'produits'=>$produit
+          ]   
+        ));   
         dd($produit);
+        // $produit correspond à l'entité produit de l'identifiant envoyé
+        // en parametre
+     
         return $this->render('detailproduit/index.html.twig', [
             'controller_name' => 'DetailproduitController',
         ]);
