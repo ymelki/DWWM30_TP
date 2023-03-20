dc1c2a8b7f1d3f0371315c9559b2abe4dde7c751
Melki <ymelki@gmail.com>
2023-03-20 16:55:26 +0100

    Ajout_d'un_controlleur_personnalisé

--- src/Controller/DetailproduitController.php
+++ src/Controller/DetailproduitController.php
@@ -3,7 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
-use App\Repository\CommentaireRepository;
+ use App\Repository\CommentaireRepository;
+use App\Form\ProduitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,22 +14,25 @@ class DetailproduitController extends AbstractController
     #[Route('/detailproduit/{id}', name: 'app_detailproduit')]
     public function index(Produit $produit , CommentaireRepository $commentaireRepository): Response
     {
+         $form = $this->createForm(ProduitType::class);
+
         // on veut afficher les commentaires 
         // correspondant aux produit de l'id de l'url.
         // on utilise le repository qui va chercher avec un critere findby
         // selon le produits 
-        dd($commentaireRepository->findAll());
-        dd($commentaireRepository->findBy([
+        // $toutlescommenaire=$commentaireRepository->findAll();
+        $commentaireparproduit=$commentaireRepository->findBy([
 
             'produits'=>$produit
           ]   
-        ));   
-        dd($produit);
+        );   
+
         // $produit correspond à l'entité produit de l'identifiant envoyé
         // en parametre
-     
-        return $this->render('detailproduit/index.html.twig', [
-            'controller_name' => 'DetailproduitController',
-        ]);
+         return $this->renderForm('detailproduit/index.html.twig', [
+            'produit' => $produit,
+            'les_commentaires' => $commentaireparproduit,
+            'form' => $form
+         ]);
     }
 }
