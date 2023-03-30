c2d2fe3b7961708718d990d08b8421df073b6f71
Melki <ymelki@gmail.com>
2023-03-30 10:13:52 +0200

    paiement stripe

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use Stripe\Stripe;
 use App\Entity\Commande;
-use App\Repository\CommandeRepository;
+use Stripe\PaymentIntent;
+use Stripe\Checkout\Session;
 use App\Repository\ProduitRepository;
+use App\Repository\CommandeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CommandeController extends AbstractController
 {
@@ -31,7 +34,7 @@ class CommandeController extends AbstractController
         // this->getUser()
 
         // recuperation du panier
-        $panier=$session->getSession()->get("panier");
+    /*    $panier=$session->getSession()->get("panier");
 
         // boucler sur chaque ligne du panier
         foreach ($panier as $key => $value  ){ 
@@ -42,9 +45,41 @@ class CommandeController extends AbstractController
             $commande->setQuantite($value);
             $commandeRepository->save($commande, true);
 
-        }
+        }*/
+
+
+        //1. Payer sur STRIPE
+        // communiquer avec stripe
+
+        // clé secrete pour que stripe me reconnaisse
+        $stripeSecretKey="sk_test_51KqHUhHxTuewjfx8W4mdPLu0MLeDPM0uBpINTS0lv1lxUEkSOfK7UXbvOK8WtTFUNau0cB4hKVk4FPTMfmSSZZZh00vo9JBk6o";
+        Stripe::setApiKey($stripeSecretKey);
+
+        $YOUR_DOMAIN="http://127.0.0.1:8000";
+
+        // on lance la communication avec stripe
+        $checkout_session = Session::create([
+            'line_items' => [[
+              # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+              'amount' => '10000',
+              'quantity' => 1,
+              'currency'=>'eur',
+              'name'=>'test'
+            ]],
+            'mode' => 'payment',
+            'success_url' => $YOUR_DOMAIN . '/success.html',
+            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+          ]);
+
+          dd($checkout_session);
+
+
+          
+        //2. Savegarde en B.D.
+
+        //3. Supprimer la session
   
-        dd("test");
+        
         return $this->render('commande/index.html.twig', [
             'controller_name' => 'CommandeController',
         ]);
